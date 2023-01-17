273168b039012dff7500ab4aa005dfd3dac6519d
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-01-17 13:43:20 -0500

    Use libraries

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -8,7 +8,9 @@ defmodule Issues.CLI do
   """
   @spec run([binary]) :: :help | {binary, binary, integer}
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -39,5 +41,17 @@ defmodule Issues.CLI do
 
   def args_to_internal_representation(_) do
     :help
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [ count | #{@default_count}]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
