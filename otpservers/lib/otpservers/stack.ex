d5c9f3d3250a0f05132d2b76a3aacaf482ec7f5e
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-02-13 11:16:03 -0500

    pop

--- otpservers/lib/otpservers/stack.ex
+++ otpservers/lib/otpservers/stack.ex
@@ -17,7 +17,12 @@ defmodule Stack do
   def handle_call(:pop, _from, [head | tail]) do
     {:reply, head, tail}
   end
+
+  def handle_cast({:push, new_value}, stack) do
+    {:noreply, [new_value | stack]}
+  end
 end
 
 # { :ok, pid} = GenServer.start_link(Stack, [5, "cat", 9])
 # GenServer.call(pid, :pop)
+# GenServer.cast(pid, {:push, 42})
